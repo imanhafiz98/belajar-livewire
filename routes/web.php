1853c7636b338cb4a51ed8ec53d8d28e4cfa89fd
imanhafiz98 <imanhafiz333@gmail.com>
2021-10-25 12:46:54 +0800

    Create home, login components & add app.blade file with layouts folder

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/' , App\Http\Livewire\Home::class);
+
+Route::get('/login' , App\Http\Livewire\Login::class);
